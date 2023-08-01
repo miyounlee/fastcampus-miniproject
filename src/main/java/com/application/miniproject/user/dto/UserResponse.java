19104ececf85d4e517d1cd782b1812982de7f81b
youngjun <sohn919@naver.com>
2023-08-01 14:03:16 +0900

    [Server] #16 feat: Login, email check 응답 DTO 생성

--- src/main/java/com/application/miniproject/user/dto/UserResponse.java
+++ src/main/java/com/application/miniproject/user/dto/UserResponse.java
@@ -1,4 +1,23 @@
 package com.application.miniproject.user.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+
 public class UserResponse {
+
+    @Getter
+    @Builder
+    public static class LoginDTO {
+        private Long userId;
+        private String username;
+        private String email;
+        private String imageUrl;
+        private String accessToken;
+    }
+
+    @Getter
+    @Builder
+    public static class DuplicateEmailDTO {
+        private boolean responseType;
+    }
 }
