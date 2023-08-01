d6e92d78d0f75b4249f546342a7cc7b23c8e64be
youngjun <sohn919@naver.com>
2023-08-01 14:04:03 +0900

    [Server] #16 feat: Service Login, Join, Duplicate Email 기능 추가

--- src/main/java/com/application/miniproject/user/UserService.java
+++ src/main/java/com/application/miniproject/user/UserService.java
@@ -1,4 +1,65 @@
 package com.application.miniproject.user;
 
+import com.application.miniproject._core.security.JwtProvider;
+import com.application.miniproject.user.dto.UserRequest;
+import com.application.miniproject.user.dto.UserResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
 public class UserService {
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final UserRepository userRepository;
+    private final JwtProvider jwtProvider;
+
+    /**
+     * 회원가입
+     * TODO 이메일 중복 체크 Custom Exception
+     */
+    @Transactional
+    public void joinUser(UserRequest.JoinDTO joinDTO) {
+        if (userRepository.findByEmail(joinDTO.getEmail()).isPresent()) {
+            throw new RuntimeException("이메일 중복");
+        }
+        String password = bCryptPasswordEncoder.encode(joinDTO.getPassword());
+        userRepository.save(joinDTO.toCipherEntity(password));
+    }
+
+    /**
+     *  로그인
+     *  TODO 로그인 에러 custom exception
+     */
+    @Transactional(readOnly = true)
+    public UserResponse.LoginDTO loginUser(UserRequest.LoginDTO loginDTO) {
+        User user = userRepository.findByEmail(loginDTO.getEmail())
+                .orElseThrow(() -> new RuntimeException("로그인 에러"));
+
+        boolean passwordMatches = bCryptPasswordEncoder.matches(loginDTO.getPassword(), user.getPassword());
+
+        if (!passwordMatches) {
+            throw new RuntimeException("로그인 에러");
+        }
+        return UserResponse.LoginDTO.builder()
+                .userId(user.getId())
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .imageUrl(user.getImageUrl())
+                .accessToken(jwtProvider.generateToken(user))
+                .build();
+    }
+
+    /**
+     *  이메일 검사
+     */
+    @Transactional(readOnly = true)
+    public UserResponse.DuplicateEmailDTO isEmailUser(UserRequest.EmailDTO emailDTO) {
+        boolean isEmailUser = userRepository.findByEmail(emailDTO.getEmail()).isEmpty();
+        return UserResponse.DuplicateEmailDTO.builder()
+                .responseType(isEmailUser)
+                .build();
+    }
 }
