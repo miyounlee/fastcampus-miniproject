1b09a7832936fee103ede6c97f99047b5ff07e44
youngjun <sohn919@naver.com>
2023-08-01 14:03:47 +0900

    [Server] #16 feat: Controller Login, Join, Duplicate Email api 추가

--- src/main/java/com/application/miniproject/user/UserController.java
+++ src/main/java/com/application/miniproject/user/UserController.java
@@ -1,50 +1,40 @@
 package com.application.miniproject.user;
 
-import com.application.miniproject._core.security.JwtProvider;
-import com.application.miniproject._core.security.MyUserDetails;
 import com.application.miniproject.user.dto.UserRequest;
+import com.application.miniproject.user.dto.UserResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
+import javax.validation.Valid;
 
-@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/user")
 @RestController
 public class UserController {
 
-    private final UserRepository userRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
-    private final JwtProvider jwtProvider;
+    private final UserService userService;
 
     @PostMapping("/join")
-    public ResponseEntity<?> join(@RequestBody UserRequest.JoinDTO joinDTO) {
-        String password = bCryptPasswordEncoder.encode(joinDTO.getPassword());
-        joinDTO.setPassword(password);
-
-        userRepository.save(joinDTO.toEntity());
+    public ResponseEntity<?> join(@Valid @RequestBody UserRequest.JoinDTO joinDTO) {
+        userService.joinUser(joinDTO);
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody UserRequest.LoginDTO loginDTO) {
-        Optional<User> userOP = userRepository.findByEmail(loginDTO.getEmail());
+    public ResponseEntity<?> login(@Valid @RequestBody UserRequest.LoginDTO loginDTO) {
+        UserResponse.LoginDTO response = userService.loginUser(loginDTO);
 
-        String jwt = jwtProvider.generateToken(userOP.get());
-
-        return ResponseEntity.ok().header("Authorization", "Bearer " + jwt).build();
+        return ResponseEntity.ok().body(response);
     }
 
-    @GetMapping("/test")
-    public ResponseEntity<?> test(@AuthenticationPrincipal MyUserDetails myUserDetails) {
-        System.out.println(myUserDetails.getUsername());
-        return ResponseEntity.ok().body("ok");
+
+    @PostMapping("/email")
+    public ResponseEntity<?> email(@Valid @RequestBody UserRequest.EmailDTO emailDTO) {
+        UserResponse.DuplicateEmailDTO duplicateEmailDTO = userService.isEmailUser(emailDTO);
+
+        return ResponseEntity.ok().body(duplicateEmailDTO);
     }
 }
