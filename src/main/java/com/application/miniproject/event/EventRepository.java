6715a1b4ced04ea4e0355424074a73f49cc5c670
Miyeon Lee <leeyun92@naver.com>
2023-08-03 19:55:35 +0900

    [Server] #31 feat : 연차/당직 신청 시 검증 로직 추가

--- src/main/java/com/application/miniproject/event/EventRepository.java
+++ src/main/java/com/application/miniproject/event/EventRepository.java
@@ -1,5 +1,6 @@
 package com.application.miniproject.event;
 
+import com.application.miniproject.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,6 +14,15 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     @Query("SELECT e FROM Event e WHERE e.eventType = 'DUTY' AND e.startDate = :dutyDate AND e.orderState = 'APPROVED'")
     Optional<Event> findByDutyDate(@Param("dutyDate") LocalDate dutyDate);
 
-    @Query("SELECT e FROM Event e LEFT JOIN e.user u WHERE u.id = :id")
     List<Event> findAllByUserId(@Param("id") Long id);
+
+    @Query("SELECT e FROM Event e WHERE e.user = :user AND " +
+            "(:startDate >= e.startDate AND :startDate <= e.endDate) " +
+            "OR (:endDate >= e.startDate AND :endDate <= e.endDate) " +
+            "OR (:startDate = e.startDate OR :startDate = e.endDate) " +
+            "OR (:endDate = e.startDate OR :endDate = e.endDate) " +
+            "OR (e.startDate >= :startDate AND e.startDate <= :endDate)" +
+            "OR (e.endDate >= :startDate AND e.endDate <= :endDate)")
+    Optional<Event> findDuplicatedEvent(@Param("user") User user, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
+
 }
