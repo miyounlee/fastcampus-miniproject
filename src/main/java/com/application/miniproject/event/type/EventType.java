d5905e135ef5bb6d5c9725c5c36b349bffcdb5fd
Miyeon Lee <leeyun92@naver.com>
2023-08-07 14:16:31 +0900

    [Server] modify : enum 클래스 수정

--- src/main/java/com/application/miniproject/event/type/EventType.java
+++ src/main/java/com/application/miniproject/event/type/EventType.java
@@ -1,15 +1,10 @@
 package com.application.miniproject.event.type;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
 @Getter
 public enum EventType {
 
-    LEAVE("연차"),
-    DUTY("당직");
-
-    private final String type;
+    LEAVE, DUTY
 
 }
--- src/main/java/com/application/miniproject/event/type/OrderState.java
+++ src/main/java/com/application/miniproject/event/type/OrderState.java
@@ -1,16 +1,10 @@
 package com.application.miniproject.event.type;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
-@AllArgsConstructor
 @Getter
 public enum OrderState {
 
-    WAITING("승인 대기"),
-    APPROVED("승인 완료"),
-    REJECTED("승인 반려");
+    WAITING, APPROVED, REJECTED
 
-    private final String state;
 }
