90565d3ad29cdcebbe2f57f4557b3b790d633068
Miyeon Lee <leeyun92@naver.com>
2023-08-26 00:23:20 +0900

    [Server] refactor : EventRepository 쿼리 메소드 사용으로 변경, Service 중복 코드 제거에 따른 리팩토링

--- src/main/java/com/application/miniproject/event/EventRepository.java
+++ src/main/java/com/application/miniproject/event/EventRepository.java
@@ -1,6 +1,7 @@
 package com.application.miniproject.event;
 
-import com.application.miniproject.user.User;
+import com.application.miniproject.event.type.EventType;
+import com.application.miniproject.event.type.OrderState;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,22 +12,13 @@ import java.util.Optional;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
 
-    @Query("SELECT e FROM Event e WHERE e.eventType = 'DUTY' AND e.startDate = :startDate AND e.orderState = 'APPROVED'")
-    Optional<Event> findByDutyDate(@Param("startDate") LocalDate startDate);
+    Optional<Event> findByEventTypeAndStartDateAndOrderState(@Param("eventType") EventType eventType, @Param("startDate") LocalDate startDate, @Param("orderState") OrderState orderState);
 
     List<Event> findAllByUserId(@Param("id") Long id);
 
-    @Query("SELECT e FROM Event e LEFT JOIN e.user u WHERE u.id = :id AND e.orderState != 'REJECTED' AND " +
-            ":startDate BETWEEN e.startDate AND e.endDate")
-    Optional<Event> findByMyDutyDate(@Param("id") Long id, @Param("startDate") LocalDate startDate);
-
     @Query("SELECT e FROM Event e LEFT JOIN e.user u " +
-            "WHERE u.id = :id AND e.orderState != 'REJECTED' AND " +
-            ":startDate BETWEEN e.startDate AND e.endDate")
-    Optional<Event> findByLeaveDate(@Param("id") Long id, @Param("startDate") LocalDate startDate);
-
-    @Query("SELECT e FROM Event e LEFT JOIN e.user u WHERE u.id = :id AND e.orderState != 'REJECTED' AND " +
-            "(e.startDate <= :endDate AND e.endDate >= :startDate)")
-    List<Event> findByLeaveDates(@Param("id") Long id, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
+            "WHERE u.id = :id AND e.orderState != 'REJECTED' " +
+            "AND ((:endDate BETWEEN e.startDate AND e.endDate) OR (:startDate BETWEEN e.startDate AND e.endDate))")
+    List<Event> findEvent(@Param("id") Long id, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 
 }
